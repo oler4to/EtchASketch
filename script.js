faf13ee6592b5d1caf383ab955db8df632bcd30b
oleolerato <olerato.mosikidi@gmail.com>
2022-08-17 12:21:06 +0200

    Declare variables; defaults and currents for grid modes and cell colors. Declare rgb variable for color.

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const buttons = document.querySelector('#buttons')
 const createGrid = document.createElement('button')
 const defaultButton = document.createElement('button')
 const colorButton = document.createElement('button')
+
+let defaultDimensions = 16
+let defaultMode = 'default'
+let defaultColor = '#2f2f2f'
+let rgb = ''
+let currentColor = defaultColor
+let currentMode = defaultMode
+
